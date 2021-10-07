fb494d2279780d663769f36805812795e7d27af4
Abdulaziz Almekhlafi <Abdulazuz716111@gmail.com>
2021-10-07 15:06:45 +0300

    Home is set.

--- js/script.js
+++ js/script.js
@@ -8,5 +8,12 @@ menu.onclick = () => {
 window.scroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
+};
+
+document.querySelectorAll('.image-slider img').forEach(images => {
+    images.onclick = () =>{
+        var src = images.getAttribute('src');
+        document.querySelector('.main-home-img').src = src;
+    }
+});
 
